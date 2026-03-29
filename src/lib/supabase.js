a0ba988ba14706f84071b4ee9e03664270939b56
Donezo <test@test.com>
2026-03-29 18:30:21 +0530

    env variables setup

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://betsqexkpcxovwlgtqhw.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_cWK9X5dVrTb0h-MzKOjIUg_xbDTJuPt'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     auth: {
